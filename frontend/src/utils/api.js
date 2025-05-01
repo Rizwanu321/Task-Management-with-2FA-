194bcd045f19319f1c05f8e0c5b0c480c480aa07
Rizwanu321 <142315620+Rizwanu321@users.noreply.github.com>
2025-05-01 15:10:19 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import { getToken } from "./auth";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://task-management-with-2fa-backend.onrender.com/api";
 
 const getHeaders = () => {
   const token = getToken();
