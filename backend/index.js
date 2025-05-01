a2dae18b5fc4ac2bf54c294524392f517e7a51d2
Rizwanu321 <142315620+Rizwanu321@users.noreply.github.com>
2025-05-01 15:19:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const taskRoutes = require("./routes/taskRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:3000"];
+const allowedOrigins = [process.env.FRONTEND_URL || "https://task-management-with-2fa.onrender.com"];
 
 app.use(
   cors({
